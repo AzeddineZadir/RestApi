37259479f29dd171fe8c294287871e0b099ee0be
silvermoon06 <33894689+silvermoon06@users.noreply.github.com>
2020-02-22 00:01:48 +0100

    correction update.php

--- api/category/update.php
+++ api/category/update.php
@@ -18,6 +18,8 @@
   $data = json_decode(file_get_contents("php://input"));
 
   // Set ID to UPDATE
+  $category->name = $data->name;
+  $category->firstname = $data->fisrstname;
   $category->id = $data->id;
 
   // Delete post
